7e9c68d3d87ecad9f03afec3fbb5aadac0db5b60
PrathamRawat <prathamrawat815@gmail.com>
2021-12-04 21:06:24 -0500

    Add more scraping code

--- scraping.py
+++ scraping.py
@@ -12,6 +12,10 @@ songlist = pd.read_csv("data/trimmed_billboard_songs.csv")
 songlist.drop(songlist.columns.difference(
     ['Unnamed: 0', 'artist', 'song']), 1, inplace=True)
 
+print(songlist.shape)
+songlist = songlist.drop_duplicates(["artist", "song"])
+print(songlist.shape)
+
 
 def scrapegenius(song, artist):
     songname = LyricsGenius.search_song(song, artist)
@@ -20,14 +24,51 @@ def scrapegenius(song, artist):
 
 
 data = pd.DataFrame(columns=['Name', 'Lyrics'])
+errors = pd.DataFrame(columns=['song', 'artist'])
 
-for id in songlist['Unnamed: 0'][:10]:
+
+for id in songlist['Unnamed: 0']:
     song = songlist['song'][id]
     artist = songlist['artist'][id]
+    try:
+        lyrics = scrapegenius(song, artist)
 
-    lyrics = scrapegenius(song, artist)
+        new_row = {'Name': song, 'Lyrics': lyrics}
+        data = data.append(new_row, ignore_index=True)
+    except Exception as e:
+        errors = errors.append({"song": song, "artist": artist}, ignore_index=True)
+        print(e)
 
-    new_row = {'Name': song, 'Lyrics': lyrics}
-    data = data.append(new_row, ignore_index=True)
+data.to_csv("data/billboard-lyrics.csv", index=False)
+errors.to_csv("data/billboard-errors.csv", index=False)
 
-data.to_csv("data/lyrics.csv", index=False)
+
+
+
+songlist = pd.read_csv("data/trimmed_non_billboard_songs.csv")
+
+songlist.drop(songlist.columns.difference(
+    ['Unnamed: 0', 'artist', 'song']), 1, inplace=True)
+
+print(songlist.shape)
+songlist = songlist.drop_duplicates(["artist", "song"]).sample(15000)
+print(songlist.shape)
+
+data = pd.DataFrame(columns=['Name', 'Lyrics'])
+errors = pd.DataFrame(columns=['song', 'artist'])
+
+
+for id in songlist['Unnamed: 0']:
+    song = songlist['song'][id]
+    artist = songlist['artist'][id]
+    try:
+        lyrics = scrapegenius(song, artist)
+
+        new_row = {'Name': song, 'Lyrics': lyrics}
+        data = data.append(new_row, ignore_index=True)
+    except Exception as e:
+        errors = errors.append({"song": song, "artist": artist}, ignore_index=True)
+        print(e)
+
+data.to_csv("data/non-billboard-lyrics.csv", index=False)
+errors.to_csv("data/non-billboard-errors.csv", index=False)
