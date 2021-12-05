725c68b9c1d9836b611e054b6442c33023cde2e3
Pun2341 <67458426+Pun2341@users.noreply.github.com>
2021-12-04 20:58:46 -0500

    remove brackets when scraping

--- scraping.py
+++ scraping.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 from pandas.core.frame import DataFrame
 from api_key import client_access_token
+import re
 
 LyricsGenius = lyricsgenius.Genius(client_access_token)
 
@@ -18,7 +19,6 @@ def scrapegenius(song, artist):
     lyrics = songname.lyrics
     return lyrics
 
-
 data = pd.DataFrame(columns=['Name', 'Lyrics'])
 
 for id in songlist['Unnamed: 0'][:10]:
@@ -26,8 +26,7 @@ for id in songlist['Unnamed: 0'][:10]:
     artist = songlist['artist'][id]
 
     lyrics = scrapegenius(song, artist)
-
-    new_row = {'Name': song, 'Lyrics': lyrics}
+    new_row = {'Name': song, 'Lyrics': re.sub("([\[]).*?([\)\]])", "", lyrics)}
     data = data.append(new_row, ignore_index=True)
 
 data.to_csv("data/lyrics.csv", index=False)
